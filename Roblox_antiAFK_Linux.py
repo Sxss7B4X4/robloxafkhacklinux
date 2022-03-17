bfec7e8e91005ddda10a423745f25af61627aba7
Sxss7B4X4 <98440714+Sxss7B4X4@users.noreply.github.com>
2022-03-17 15:27:01 +0300

    Add files via upload

---
+++ Roblox_antiAFK_Linux.py
@@ -0,0 +1,56 @@
+################################################
+# Bu Bir Test Buildidir                        #
+# Bu Build Alyx CC Ve Tayfasi Icin Yapilmistir #
+################################################
+
+
+
+import time
+import os
+from pynput.keyboard import Key, Controller
+keyboard = Controller()
+os.system("clear")
+print("""
+=========================
+    ROBLOX AFK HACK
+     LINUX EDITION
+     
+      1 - TESTING
+      2 - AFK HACK
+      3 - HELP 
+=========================""")
+choice=input("Roblox AFK Hack You Select A Choice And Please Press Enter : ")
+
+if choice =='1':
+   time.sleep(1)
+   keyboard.press("x")
+   print("Hack Is Working And Stable")
+   time.sleep(1)
+
+if choice =='2':
+   print("Hack Is Activated GLHF Comrade")
+   while True:
+        keyboard.press('a')
+        time.sleep(1)
+        keyboard.press('s')
+        time.sleep(1)
+        keyboard.press('d')
+        time.sleep(1)
+        keyboard.press('w')
+        time.sleep(1)
+if choice =='3':
+   os.system("clear")
+   print("""======================================================
+                       How To Use
+
+            
+            1 - Start Game
+            2 - Open Hack
+            3 - Select 2 And Press Enter
+            4 - Enjoy Comrade
+         This Window Is Auto Exit In 10 Seconds
+=======================================================""")
+   time.sleep(10)
+
+# Devami Yakinda Gelecek Beklemede Kalin
+# Created By ZozamadX#0424
